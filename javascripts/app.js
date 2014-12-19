0ad80e8f5b67179b1c67af6c7ce8c07a71048d23
thetlk <jeremieboutoille@gmail.com>
2014-12-19 11:27:08 +0100

    Add login controller

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,28 @@
-(function(){
 
-    var app = angular.module("SopraCovoit", ['ngMaterial']);
+'use strict';
 
-    app.controller("SopraCovoit", ["$scope", function($scope){
+var sopracovoitApp = angular.module('sopracovoitApp', [
+    'ngMaterial',
+    'ngRoute',
+    'sopracovoitControllers'
+]);
 
-    }]);
+sopracovoitApp.config(['$routeProvider', function($routeProvider){
+    $routeProvider.
+        when('/login', {
+            templateUrl: 'partials/login.html',
+            controller: 'LoginCtrl'
+        }).
+        otherwise({
+            redirectTo: '/stats'
+        });
+}]);
 
-})();
+sopracovoitApp.run(function($rootScope, $location){
+   $rootScope.$on('$routeChangeStart', function(event, next, current){
+      if($rootScope.loggedUser == null && next.originalPath != "/login")
+      {
+          $location.path('/login');
+      }
+   });
+});
---
+++ javascripts/controllers.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var sopracovoitControllers = angular.module('sopracovoitControllers', []);
+
+sopracovoitControllers.controller('LoginCtrl', ["$scope", function($scope){
+    $scope.bloub = "ici";
+}]);
